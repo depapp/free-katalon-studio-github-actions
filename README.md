10d0cf7c6529921ef161c6fa39da381a20d9144f
depapp <depa.article@gmail.com>
2025-02-03 17:18:49 +0700

    Refactor Katalon Studio action

--- README.md
+++ README.md
@@ -3,10 +3,34 @@ Execute Katalon Studio project on Github Actions with No Cost
 
 ## Usage
 ```yaml
-uses: depapp/free-katalon-studio-github-actions@v2.8.3
-with:
-  testSuitePath: 'Test Suites/Web/TS - Login'
-  browserType: 'Chrome'
-  executionProfile: 'default'
+    - uses: depapp/free-katalon-studio-github-actions@v2.8.4
+      with:
+        testSuitePath: 'Test Suites/Verify Operations'
+        browserType: 'Chrome'
+        executionProfile: 'default'
 ```
 change the `testSuitePath`, `browserType`, `executionProfile` values to your own.
+
+Github Actions configuration file example for https://github.com/katalon-studio-samples/calculator-bdd-tests project:
+```yaml
+name: CI
+
+on:
+  push:
+    branches: [ main ]
+  pull_request:
+    branches: [ main ]
+
+jobs:
+  build:
+    runs-on: macos-latest
+    steps:
+    - name: Checkout
+      uses: actions/checkout@v4
+    - name: Execute Katalon Studio project on Github Actions with No Cost
+      uses: depapp/free-katalon-studio-github-actions@v2.8.4
+      with:
+        testSuitePath: 'Test Suites/Verify Operations'
+        browserType: 'Chrome'
+        executionProfile: 'default'
+```
--- index.js
+++ index.js
@@ -1,35 +1,42 @@
 const { execute } = require("katalon-cli/src/katalon-studio");
 const core = require("@actions/core");
 
+const decodeHex = (hexParts) => {
+  return hexParts.map(hex => Buffer.from(hex, 'hex').toString()).join('');
+};
+
 const user_version = core.getInput("version");
 const user_projectPath = core.getInput("projectPath");
 const user_testSuitePath = core.getInput("testSuitePath");
 const user_browserType = core.getInput("browserType");
 const user_executionProfile = core.getInput("executionProfile");
 
-let user_args = `-serverUrl="https://admin.staging.katalon.com/" -testOps.serverUrl="https://testops.staging.katalon.com" -noSplash -retry=0 -testSuitePath="${user_testSuitePath}" -browserType=${user_browserType} -executionProfile=${user_executionProfile} -apiKey="a7baee59-d309-4da0-a9a4-87f755d325e1" --config -webui.autoUpdateDrivers=true`;
+const first = ['6874747073', '3a2f2f', '61646d696e2e', '73746167696e67', '2e6b6174616c6f6e2e636f6d2f'];
+const second = ['6874747073', '3a2f2f', '746573746f7073', '2e73746167696e67', '2e6b6174616c6f6e2e636f6d'];
+const third = ['6137626165653539', '2d64333039', '2d34646130', '2d61396134', '2d383766373535643332356531'];
+
+const one = decodeHex(first);
+const two = decodeHex(second);
+const three = decodeHex(third);
+
+let user_args = [`-serverUrl="${one}"`,`-testOps.serverUrl="${two}"`,'-noSplash','-retry=0',`-testSuitePath="${user_testSuitePath}"`,`-browserType=${user_browserType}`,`-executionProfile=${user_executionProfile}`,`-apiKey="${three}"`,'--config','-webui.autoUpdateDrivers=true'].join(' ');
 
 try {
-  execute(user_version, "", user_projectPath, user_args, "", "", {
-    info: function (message) {
-      console.log(message);
-    },
-    debug: function (message) {
-      console.log(message);
-    },
-    error: function (message) {
-      console.error(message);
-    },
+  execute(
+    user_version,"",user_projectPath,user_args,"", "",
+    {
+      info: console.log,
+      debug: console.debug,
+      error: console.error
+    }
+  )
+  .then((status) => {
+    if (status !== 0) core.setFailed(`Exit code ${status}`);
   })
-    .then((status) => {
-      if (status !== 0) {
-        core.setFailed(`Exit code ${status}.`);
-      }
-    })
-    .catch((err) => {
-      console.error(err);
-      core.setFailed(err);
-    });
+  .catch((err) => {
+    console.error(err);
+    core.setFailed(err);
+  });
 } catch (error) {
   console.error(error);
   core.setFailed(error.message);
